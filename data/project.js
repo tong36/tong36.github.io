4a8a6c2508a1b4b8013f2c9e9d6a338215995234
tong36 <jimaoshitang@gmail.com>
2016-10-25 18:35:53 +0800

    add personal cv

---
+++ data/project.js
@@ -0,0 +1,52 @@
+
+/*		项目池
+	*   {
+	*       projectName: @value String            *         项目名称
+	*       projectWebsite: @value String         *         项目网址
+	*       startTime: @value String              *         项目开始时间  格式为：2016.10.09
+	*       endTime: @value String 			      *         项目结束时间  格式为：2016.10.09
+	*       projectExplain: @value String         *         项目说明
+	*       projectLabel: @value Array            *         项目技术标签
+			projectThumbnail: @value String       *         缩略图地址
+	*
+	*   }
+*/
+
+var projects = [
+		{
+			projectName:"QQ浏览器 - 网站”",
+			projectWebsite:"http://zhibo36.me/qqBrowser/",
+		    startTime:"2016.04",
+			endTime:"2016.06",
+			projectExplain:"作品介绍：视觉感很强的网站,页面动效主要运用css3 animation transition transform 等和原生js，利用js控制其动画展示位置，利用百分比实现全屏布局，封装的方法全部原生js实现。",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-1.png"
+		},
+		{
+			projectName:"坚果手机-首屏效果",
+			projectWebsite:"http://zhibo36.me/nut_phone/",
+		    startTime:"2016.04",
+			endTime:"2014.06",
+			projectExplain:"作品介绍：网页的交互效果很赞，当鼠标滑入时，这七部彩色手机随着鼠标的远近而上下跳跃，运用原生js，封装mTeens(运动框架)，配合css3，使用transform，animation等标签制作流程动画效果，实现‘图片的位移’，‘缓冲运动’，‘点击查看图片细节’，‘图片放大居中显示’等有意思的交互效果",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-2.png"
+		},
+		{
+			projectName:"music",
+			projectWebsite:"http://zhibo36.me/music/",
+		    startTime:"2016.06",
+			endTime:"至今",
+			projectExplain:"网页的交互效果非常好，主要运用原生js 配合css3开发完成，鼠标移入移出动态效果吸引眼球，点击调用指定data中的json数据跳转页面，利用原生js实现全选 删除 暂停播放等一系列功能，封装的方法全部原生js实现",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-3.png"
+		},
+		{
+			projectName:"DESIGNEAT - 视觉类",
+			projectWebsite:"http://zhibo36.me/design/",
+		    startTime:"2016.06",
+			endTime:"至今",
+			projectExplain:"作品介绍：视觉差类网站，使用原生js 配合 css3 进行开发，实现图片跟随鼠标移动，版块间视觉差，定时器，animation标签模拟gif格式动画！部分板块使用数据动态生成结构进行渲染",
+			projectLabel:["html","css3","css","js"],
+			projectThumbnail:"images/icon/project-4.png"
+		}
+]
---
+++ data/userInfo.js
@@ -0,0 +1,37 @@
+
+/*个人信息说明，数据格式为对象
+	*   {
+	*       userName: @value String               *         姓名
+			userPortrait: @value String           *         头像
+	*       jobWant: @value String                *         求职意向
+	*       userQQ: @value String|Number          *         QQ号码
+	*       userEmail: @value String              *         邮箱地址
+	*       motto: @value String                  []         激励格言
+	*       userPhone: @value String|Number       *          手机号码
+	*       userOrignPlace: @value String         *         籍贯
+	*       userSeatPlace: @value String          *         所在地
+	*       userWeibo：@value String              []         微博地址
+	*       userAssessment: @value String         []        自我评价
+	*       userHobby: @value Array              []          爱好
+	*       userAward:@value Array               []          奖项
+	*
+	*
+	*   }
+*/
+
+var userInfo = {
+    userName: "杨智博" , 
+    userPortrait:"images/icon/intro_03.png",
+    jobWant:  "WEB前端开发工程师",
+    userQQ:  "865485520",
+    userEmail:   "jimaoshitang@gmail.com",
+    motto:  "如果你浪费了自己的年龄，那是挺可悲的。因为你的青春只能持续一点儿时间——很短的一点儿时间。", 
+    userPhone: 15210795251,
+    userOrignPlace:  "陕西省渭南市" ,
+    userSeatPlace:  "北京市",
+    userWeibo: "",
+    userAssessment: "个人工作态度端正，有良好进取心，同时自学能力强，能很快适应新的环境，为人和善",   
+    userHobby: ["运动","电影","游戏"],
+//  userAward: ["最佳前端架构奖"]
+}
+
